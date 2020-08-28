2f98ce1d9bc02d48787199a817f2e6376b139fda
Shahrzad <shahrzadshirzad@gmail.com>
2020-08-28 10:45:14 -0500

    update python scripts for plots and plots

--- python_scripts/test_mp.py
+++ python_scripts/test_mp.py
@@ -12,24 +12,47 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 import torch.multiprocessing as mp
-
+import time
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+import argparse
+import sys
+
+if not len(sys.argv) == 6 :
+    print("This program requires the following 4 arguments seperated by a space, channel is set to 3 by default ")
+    print("batch length kernel_size out_channels")
+    exit(-57)
+
+parser = argparse.ArgumentParser(description='Parameters')
+parser.add_argument('integers', type=int, nargs=5,
+                    help='batch, length, kernel_size, out_channels')
+
+
+args = parser.parse_args()
+#print("Command Line: " ,args.integers[0], args.integers[1], args.integers[2], args.integers[3])
+
+batch = args.integers[0]
+length = args.integers[1]
+kernel_size = args.integers[2]
+out_channels = args.integers[3]
+num_cores = args.integers[4]
 
 def setup(rank, world_size):
+    os.environ["CUDA_VISIBLE_DEVICES"]=""
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
-    dist.init_process_group("mpi", rank=rank, world_size=world_size)
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 
 def cleanup():
     dist.destroy_process_group()
+    
 class ToyModel(nn.Module):
     def __init__(self):
         super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
+        self.net1 = nn.Conv1d(in_channels=3,out_channels=out_channels, kernel_size=kernel_size, bias=False)
 
     def forward(self, x):
         return self.net1(x)
@@ -40,18 +63,18 @@ def demo_basic(rank, world_size):
     setup(rank, world_size)
 
     # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-
+    model = ToyModel()
+    t=time.time()
+    ddp_model = DDP(model, device_ids=None, output_device=None)
+    print(time.time()-t)
 #    loss_fn = nn.MSELoss()
 #    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 #
 #    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
+    outputs = ddp_model(torch.randn((batch, 3, length),requires_grad=False))
 #    labels = torch.randn(20, 5).to(rank)
 #    loss_fn(outputs, labels).backward()
 #    optimizer.step()
-
     cleanup()
 
 
@@ -62,4 +85,6 @@ def run_demo(demo_fn, world_size):
              join=True)
 
 if __name__ == "__main__":    
-    run_demo(demo_basic, 8)
+    t1=time.time()
+    run_demo(demo_basic, num_cores)
+    print(time.time() - t1)
--- python_scripts/torch_test.py
+++ python_scripts/torch_test.py
@@ -12,6 +12,7 @@ import os
 import torch
 import torch.distributed as dist
 from torch.multiprocessing import Process
+import torch.nn as nn
 
 def run(rank, size):
     """ Distributed function to be implemented later. """
@@ -35,3 +36,34 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
+        
+@torch.no_grad()
+def init_weights(m):
+    print(m)
+    if type(m) == nn.Linear:
+        m.weight.fill_(1.0)
+        print(m.weight)
+        
+net = nn.Sequential(nn.Linear(2, 2), nn.Linear(2, 2))
+net.apply(init_weights)
+
+
+with torch.no_grad():
+    array = torch.rand(10, 3, 5,requires_grad=True)
+net = nn.Conv1d(in_channels=3,out_channels=2, kernel_size=4, bias=False)
+out=net(array)
+
+def init_weights(m):
+    array = torch.rand(10, 3, 5,requires_grad=True)
+    out=m(array)
+    return out
+#    print(m)
+#    if type(m) == nn.Conv1d:
+#        m.weight.fill_(1.0)
+#        print(m.weight)
+        
+net = nn.Conv1d(in_channels=3,out_channels=2, kernel_size=4, bias=False)
+a=net.apply(init_weights)
+
+conv1 = nn.Conv1d(in_channels=3,out_channels=2, kernel_size=10, bias=False)
+conv1d.apply(init_weights)
