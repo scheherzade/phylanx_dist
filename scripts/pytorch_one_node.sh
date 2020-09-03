e4ed4f890b046f9460d4401af08c224531ab7c5e
Shahrzad Shirzad <shahrzadshirzad@gmail.com>
2020-09-03 13:18:44 -0500

    run on one node

--- scripts/pytorch_one_node.sh
+++ scripts/pytorch_one_node.sh
@@ -14,8 +14,8 @@ result_dir="/work/sshirzad/phylanx_dist/results"
 
 batch=(100000)
 length=(500)
-out_channels=(10)
-filter_length=(5)
+out_channels=(100)
+filter_length=(20)
 
 thr=(1 4 8 16 24 32 40)
 export PATH=${phylanx_bin_dir}:$PATH
